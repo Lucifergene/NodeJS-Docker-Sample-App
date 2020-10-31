2914ddfb45b7bc3dcaf492642862b4ddb6391640
Avik Kundu <1828008@kiit.ac.in>
2020-10-31 07:00:05 +0000

    Universal Open Port Config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
-const port =3000
+
+const PORT =3000
+const HOST = '0.0.0.0';
+
 var cors = require('cors')
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-});
+app.listen(PORT, HOST);
+console.log(`Example app listening at http://${HOST}:${PORT}`);
